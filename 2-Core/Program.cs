6e2066982bc2e3783e4d8b2d6642856049deafde
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-04-07 14:25:59 +0200

    introduce while and do-while

--- 2-Core/Program.cs
+++ 2-Core/Program.cs
@@ -15,37 +15,16 @@ for (int i = 42; i <= 50; i+=2)
     Console.WriteLine(i);
 }
 
-
-/* Möglich, aber in der Praxis nicht gebräuchlich:
-
 int counter = 0;
-for (; counter < 5; counter++)
+while(counter < 5)
 {
-    int j = 2 * counter;
-    Console.WriteLine(j);
+    counter += 2;
+    Console.WriteLine(counter);
 }
-Console.WriteLine(counter);
 
-int counter2 = 0;
-for (; counter2 < 5; )
+do
 {
-    int j = 2 * counter2;
-    Console.WriteLine(j);
-    counter2++;
+    counter += 2;
+    Console.WriteLine(counter);
 }
-Console.WriteLine(counter);
-
-int counter3 = 0;
-bool condition = true;
-for (; condition; )
-{
-    int j = 2 * counter3;
-    Console.WriteLine(j);
-    counter2++;
-    if (counter3 == 5)
-    {
-        condition = false;
-    }
-}
-Console.WriteLine(counter);
-*/
+while(counter < 10);
