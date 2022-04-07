562283c67f716c87ffc25e8268045b5326f1f63a
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-04-07 15:19:16 +0200

    implement string PlanetFromPosition(int position)

--- 2-Core/Program.cs
+++ 2-Core/Program.cs
@@ -1,18 +1,60 @@
-﻿string message = "Hello";
-int Sum(int i1, int i2) 
+﻿string PlanetFromPosition(int position)
 {
-    int result = i1 + i2; 
-    bool state = i1 > 5;
-    return result;
+    string planetName = "unknown";
+    if (position > 0 && position <= 9)
+    {
+
+        switch(position)
+        {
+            case 1:
+                {
+                    planetName = "Mercury";
+                    break;
+                }
+            case 2:
+                {
+                    planetName = "Venus";
+                    break;
+                }
+            case 3:
+                {
+                    planetName = "Earth";
+                    break;
+                }
+            case 4:
+                {
+                    planetName = "Mars";
+                    break;
+                }
+            case 5:
+                {
+                    planetName = "Jupiter";
+                    break;
+                }
+            case 6:
+                {
+                    planetName = "Saturn";
+                    break;
+                }
+            case 7:
+                {
+                    planetName = "Uranus";
+                    break; 
+                }
+            case 8:
+                {
+                    planetName = "Neptune";
+                    break;
+                }
+            case 9:
+                {
+                    planetName = "Pluto";
+                    break;
+                }
+        }
+    }
+    return planetName;
 }
 
-string Hugo(bool p1, string p2, int p3) 
-{
-    Console.WriteLine(p2.Length);
-    Console.WriteLine(p1.ToString());
-
-    return message;
-}
-
-int sum = Sum(4, 38);
-Console.WriteLine(sum);
+int planetPosition = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PlanetFromPosition(planetPosition));
