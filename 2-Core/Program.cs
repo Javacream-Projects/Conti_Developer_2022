348848d5c592b3b6f9454327325e7c189e3a0dfb
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-04-07 13:47:21 +0200

    implement planet switch

--- 2-Core/Program.cs
+++ 2-Core/Program.cs
@@ -1,57 +1,57 @@
-﻿//if - else
-bool state = false;
-
-string output = "state was true";
-
-if (state == false)
+﻿int planet = Convert.ToInt32(Console.ReadLine());
+if (planet > 0 && planet <= 9)
 {
-    Console.WriteLine(output);
-}
-else
-{
-    Console.WriteLine("state was false");
 
-}
-
-int i1 = 4;
-int i2 = 42;
-
-bool result = i1 < i2; 
-if (result)
-{
-    Console.WriteLine(output);
-}
-
-
-if (i1 < i2)
-{
-    Console.WriteLine("state was true");
-}
-//Korrekt, aber ungebräuchlich
-if (i1 < i2)
-    Console.WriteLine("state was true");
-
-
-//switch-case
-
-int planet = 1;
-
-switch(planet)
-{
-    case 1:
-        {
-            Console.WriteLine("Planet is Mercury");
-            break;
-        }
-    case 2:
-        {
-            Console.WriteLine("Planet is Venus");
-            break;
-        }
-    default:
+    string output = "Planet is ";
+    switch(planet)
     {
-            Console.WriteLine("Planet is unknown");
-            break;
-
+        case 1:
+            {
+                break;
+            }
+        case 2:
+            {
+                output += "Venus";
+                break;
+            }
+        case 3:
+            {
+                output += "Earth";
+                break;
+            }
+        case 4:
+            {
+                output += "Mars";
+                break;
+            }
+        case 5:
+            {
+                output += "Jupiter";
+                break;
+            }
+        case 6:
+            {
+                output += "Saturn";
+                break;
+            }
+        case 7:
+            {
+                output += "Uranus";
+                break;
+            }
+        case 8:
+            {
+                output += "Neptune";
+                break;
+            }
+        case 9:
+            {
+                output += "Pluto";
+                break;
+            }
     }
+    Console.WriteLine(output);
+}
+else{
+    Console.WriteLine("Planet is unknown");
 }
