9beeb4219f690dd5f45ee783a975702a4f5d6c28
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-04-07 13:35:03 +0200

    introduce if and switch

--- 2-Core/Program.cs
+++ 2-Core/Program.cs
@@ -1,3 +1,57 @@
-﻿string numberAsString = Console.ReadLine();
-int number = Convert.ToInt32(numberAsString);
-double number2 = Convert.ToDouble(Console.ReadLine());
+﻿//if - else
+bool state = false;
+
+string output = "state was true";
+
+if (state == false)
+{
+    Console.WriteLine(output);
+}
+else
+{
+    Console.WriteLine("state was false");
+
+}
+
+int i1 = 4;
+int i2 = 42;
+
+bool result = i1 < i2; 
+if (result)
+{
+    Console.WriteLine(output);
+}
+
+
+if (i1 < i2)
+{
+    Console.WriteLine("state was true");
+}
+//Korrekt, aber ungebräuchlich
+if (i1 < i2)
+    Console.WriteLine("state was true");
+
+
+//switch-case
+
+int planet = 1;
+
+switch(planet)
+{
+    case 1:
+        {
+            Console.WriteLine("Planet is Mercury");
+            break;
+        }
+    case 2:
+        {
+            Console.WriteLine("Planet is Venus");
+            break;
+        }
+    default:
+    {
+            Console.WriteLine("Planet is unknown");
+            break;
+
+    }
+}
