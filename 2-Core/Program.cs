9b447e561cc2dc06bf21ea1e62cec80d482f851c
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-04-07 11:07:02 +0200

    introduce explicit cast

--- 2-Core/Program.cs
+++ 2-Core/Program.cs
@@ -1,20 +1,2 @@
-﻿//typ name = literal
-string message;// = "Hello World";
-message = "Hello World";
-
-//string message = "Hugo";  //geht nicht
-//int message = 42;
-
-int number = 9;  //OK, aber da nicht verwendet sinnfrei
-
-//int number2 = 4.2;//Compiler-Fehler, ein double-Literal kann nicht einer int-Variable zugeordnet werden
-
-message = "Goodbye World";
-
-//message = 42; //Compiler-Fehler
-//message = 'H';//Compiler-Fehler
-//message = 'Hello Jupiter';//Compiler-Fehler, Character ist immer ein Einzelzeichen
-Console.WriteLine(message);
-
-const int number3 = 9;
-//number3 = 42;
+﻿double d = 4; //impliziter Cast, vom "kleineren" in den "größeren" Datentypen
+int i = (int)4.2; //(new type) expliziter Cast in die andere Richtung
