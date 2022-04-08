dcfd85d3afdb910690ddc3491554823f7f216458
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-04-08 13:29:45 +0200

    introduce try catch

--- 2-Core/Program.cs
+++ 2-Core/Program.cs
@@ -1,117 +1,77 @@
-﻿void StringExample(){
-    string value = "initial";
-    Console.WriteLine("Before: {0}", value);
-    StringExampleFunction(value);
-    Console.WriteLine("After: {0}", value);
-}
-
-void StringExampleFunction(string s){
-    Console.WriteLine("In StringExampleFunction: {0}", s);
-    s = "changed";
-    Console.WriteLine("In StringExampleFunction after change: {0}", s);
-}
-
-void StringArrayExample(){
-    string[] values = {"initial1", "initial2"};
-    foreach(string value in values)
-    {
-        Console.WriteLine("    Before: {0}", value);
-    }
-    StringArrayExampleFunction(values);
-    foreach(string value in values)
-    {
-        Console.WriteLine("    After: {0}", value);
-    }
-}
-
-void StringArrayExampleFunction(string[] array){
-    foreach(string s in array)
-    {
-        Console.WriteLine("    In StringArrayExampleFunction: {0}", s);
-    }
-    array = new string[]{"changed1", "changed2"};
-    foreach(string s in array)
-    {
-        Console.WriteLine("    In StringArrayExampleFunction after change: {0}", s);
-    }
-}
-
-void StringArrayExample2(){
-    string[] values = {"initial1", "initial2"};
-    foreach(string value in values)
-    {
-        Console.WriteLine("    Before: {0}", value);
-    }
-    StringArrayExampleFunction2(values);
-    foreach(string value in values)
-    {
-        Console.WriteLine("    After: {0}", value);
-    }
-}
-
-void StringArrayExampleFunction2(string[] array){
-    foreach(string s in array)
-    {
-        Console.WriteLine("    In StringArrayExampleFunction: {0}", s);
-    }
-    //array = new string[]{"changed1", "changed2"};
-    array[0] = "changed1";
-    foreach(string s in array)
-    {
-        Console.WriteLine("    In StringArrayExampleFunction after change: {0}", s);
-    }
-}
-
-void SimpleStringArrayExample()
+﻿string PlanetFromPosition(int position)
 {
-    string[] array1 = {"initial1", "initial2"};
-    string[] array2 = array1;
-    array1[0] = "changed1";
-    Console.WriteLine(array2[0]);
-
-}
-
-void StringRefExample(){
-    string value = "initial";
-    Console.WriteLine("Before: {0}", value);
-    StringRefExampleFunction(ref value);
-    Console.WriteLine("After: {0}", value);
-}
-
-void StringRefExampleFunction(ref string s){
-    Console.WriteLine("In StringExampleFunction: {0}", s);
-    s = "changed";
-    Console.WriteLine("In StringExampleFunction after change: {0}", s);
-}
-
-void StringArrayRefExample(){
-    string[] values = {"initial1", "initial2"};
-    foreach(string value in values)
+    string planetName = "unknown";
+    if (position > 0 && position <= 9)
     {
-        Console.WriteLine("    Before: {0}", value);
+
+        switch(position)
+        {
+            case 1:
+                {
+                    planetName = "Mercury";
+                    break;
+                }
+            case 2:
+                {
+                    planetName = "Venus";
+                    break;
+                }
+            case 3:
+                {
+                    planetName = "Earth";
+                    break;
+                }
+            case 4:
+                {
+                    planetName = "Mars";
+                    break;
+                }
+            case 5:
+                {
+                    planetName = "Jupiter";
+                    break;
+                }
+            case 6:
+                {
+                    planetName = "Saturn";
+                    break;
+                }
+            case 7:
+                {
+                    planetName = "Uranus";
+                    break; 
+                }
+            case 8:
+                {
+                    planetName = "Neptune";
+                    break;
+                }
+            case 9:
+                {
+                    planetName = "Pluto";
+                    break;
+                }
+        }
     }
-    StringArrayRefExampleFunction(ref values);
-    foreach(string value in values)
+    return planetName;
+}
+
+void Application()
+{
+    foreach (string planetPositionAsString in args)
     {
-        Console.WriteLine("    After: {0}", value);
+        try
+        {
+            int planetPosition = Convert.ToInt32(planetPositionAsString);
+            string planetName = PlanetFromPosition(planetPosition);
+            Console.WriteLine("Planet with number {0} is {1}", planetPosition, planetName);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Invalid input: {0}", planetPositionAsString);
+            Console.WriteLine(e.Message);
+        }
     }
 }
 
-void StringArrayRefExampleFunction(ref string[] array){
-    foreach(string s in array)
-    {
-        Console.WriteLine("    In StringArrayExampleFunction: {0}", s);
-    }
-    array = new string[]{"changed1", "changed2"};
-    foreach(string s in array)
-    {
-        Console.WriteLine("    In StringArrayExampleFunction after change: {0}", s);
-    }
-}
-
-//StringExample();
-//StringArrayExample();
-//StringArrayExample2();
-//SimpleStringArrayExample();
-//StringRefExample();
-StringArrayRefExample();
+Application();
