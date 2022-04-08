55c8bd5da5bef92b22cf220ed808c843ca9beac1
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-04-08 11:34:57 +0200

    introduce value types versus reference types

--- 2-Core/Program.cs
+++ 2-Core/Program.cs
@@ -7,11 +7,12 @@
 
 void StringExampleFunction(string s){
     Console.WriteLine("In StringExampleFunction: {0}", s);
-
+    s = "changed";
+    Console.WriteLine("In StringExampleFunction after change: {0}", s);
 }
 
 void StringArrayExample(){
-    string[] values = {"element1", "element2"};
+    string[] values = {"initial1", "initial2"};
     foreach(string value in values)
     {
         Console.WriteLine("    Before: {0}", value);
@@ -24,12 +25,55 @@ void StringArrayExample(){
 }
 
 void StringArrayExampleFunction(string[] array){
-        foreach(string s in array)
+    foreach(string s in array)
     {
         Console.WriteLine("    In StringArrayExampleFunction: {0}", s);
     }
+    array = new string[]{"changed1", "changed2"};
+    foreach(string s in array)
+    {
+        Console.WriteLine("    In StringArrayExampleFunction after change: {0}", s);
+    }
+}
+
+void StringArrayExample2(){
+    string[] values = {"initial1", "initial2"};
+    foreach(string value in values)
+    {
+        Console.WriteLine("    Before: {0}", value);
+    }
+    StringArrayExampleFunction2(values);
+    foreach(string value in values)
+    {
+        Console.WriteLine("    After: {0}", value);
+    }
+}
+
+void StringArrayExampleFunction2(string[] array){
+    foreach(string s in array)
+    {
+        Console.WriteLine("    In StringArrayExampleFunction: {0}", s);
+    }
+    //array = new string[]{"changed1", "changed2"};
+    array[0] = "changed1";
+    foreach(string s in array)
+    {
+        Console.WriteLine("    In StringArrayExampleFunction after change: {0}", s);
+    }
+}
+
+void SimpleStringArrayExample()
+{
+    string[] array1 = {"initial1", "initial2"};
+    string[] array2 = array1;
+    array1[0] = "changed1";
+    Console.WriteLine(array2[0]);
 
 }
 
+
+
 //StringExample();
-StringArrayExample();
+//StringArrayExample();
+//StringArrayExample2();
+SimpleStringArrayExample();
