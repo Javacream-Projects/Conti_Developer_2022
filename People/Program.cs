4cfe5c0ff1ae578b3e01e3bd39904cd57cce9ce7
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-04 13:49:40 +0200

    play around with functions with Array

--- People/Program.cs
+++ People/Program.cs
@@ -5,13 +5,15 @@ using System; //System-Namespace wird automatisch immer benutzt
 Person p = new Person("Sawitzki", "Rainer", 183, Gender.Male);
 Student s = new Student("Einstein", "Albert", 176, Gender.Male, "LMU");
 Worker w = new Worker("Schufter", "Hannah", 176, Gender.Female, "Integrata");
-Person ps = new Student("Bohr", "Niels", 176, Gender.Male, "LMU");
-Person pw = new Worker("Schneider", "Emil", 176, Gender.Female, "Integrata");
 
+//PrintOut(p);
+//PrintOut(s);
+//PrintOut(w);
 
-
-Console.WriteLine(p.SayHello());
-Console.WriteLine(s.SayHello());
-Console.WriteLine(w.SayHello());
-Console.WriteLine(ps.SayHello());
-Console.WriteLine(pw.SayHello());
+Person[] people = {p, s, w};
+foreach(Person person in people){
+    Console.WriteLine(person.SayHello());
+}
+void PrintOut(Person person){
+    Console.WriteLine(person.SayHello());
+}
