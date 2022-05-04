16caad6c45593163e5c6cd6c1a1cb429c104c9bd
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-04 10:22:40 +0200

    play around with oop and encapsulation, part 2

--- People/Program.cs
+++ People/Program.cs
@@ -7,3 +7,23 @@ Student s = new Student("Einstein", "Albert", 176, Gender.Male, "LMU");
 Worker w = new Worker("Schufter", "Hannah", 176, Gender.Female, "Integrata");
 s.Study();
 w.Work();
+
+s.Marry(w); //Geht!, aber die Signatur ist doch  Marry(Person partner)!
+//Aber: Ein Student/Worker ist eine Person!
+Person ps = s;
+Person pw = w;
+//Student sw = w;//Das geht nicht, ein Worker ist keine Person!
+
+//Natürlich ist ein Student/Worker/Person auch ein Object!
+Object os = s;
+Object ow = w;
+Object op = p;
+
+/*
+
+WICHTIG: Achten Sie auf den Code-Assist und den Debugger!
+
+- Welche Methoden und Attribute werden z.B. für s, ps, os angezeigt
+- Was ist im Debugger zu sehen für z.B. s, ps, os 
+
+*/
