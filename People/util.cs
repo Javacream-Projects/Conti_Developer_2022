7392b435ff287c8e0276efe27fc3befca6753b4d
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-04 13:36:04 +0200

    fix TypeHelper

--- People/util.cs
+++ People/util.cs
@@ -13,7 +13,7 @@ namespace Javacream.Util{
     public static class TypeHelper{ //Eine static class darf nur static Elemente enthalten. Für solche Helper natürlich durchaus sinnvoll. Ach ja: Console ist auch eine static class
         public static void PrintTypes(Object o){
             string tab = "";
-            Type type = o.GetType();
+            Type? type = o.GetType();
             while(type != null){
                 Console.WriteLine(tab + type);
                 tab += "...";
