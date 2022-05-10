3e84c241f5396c3086ad57cd0f245745207ac0a3
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-10 11:25:28 +0200

    implement StoreService with nested  Dictionaries

--- Publishing/StoreService.cs
+++ Publishing/StoreService.cs
@@ -1,9 +1,25 @@
+using Javacream.IsbnGenerator;
 namespace Javacream.Store{
     public class StoreService{
-        private int _stock = 42;
+        private Dictionary<string, Dictionary<Object, int>> _stock;
+        
+        public StoreService(){
+         _stock = new Dictionary<string, Dictionary<Object, int>>();
+         var itemDict = new Dictionary<Object, int>();
+         _stock.Add("books", itemDict);
+         itemDict.Add(new Isbn(1,2,3,1), 42);
+         itemDict.Add(new Isbn(1,2,3,3), 10);
+        }
 
         public int GetStock(string category, Object item){
-            return _stock;
+            try{
+                Dictionary<Object, int> itemDictionary = _stock[category];
+                int stock = itemDictionary[item];
+                return stock;
+            }
+            catch(Exception){
+                return 0;
+            }
         }
     }
 }
