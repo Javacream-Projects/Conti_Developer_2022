6beafb68187b776bf7eef8d0cc45e7171f703dee
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-05-10 11:32:11 +0200

    implement StoreService with custom key

--- Publishing/StoreService.cs
+++ Publishing/StoreService.cs
@@ -1,20 +1,41 @@
 using Javacream.IsbnGenerator;
 namespace Javacream.Store{
     public class StoreService{
-        private Dictionary<string, Dictionary<Object, int>> _stock;
+        class StoreEntry{
+            public StoreEntry(string cat, Object item){
+                this._category = cat;
+                this._item = item;
+            }
+            private string _category;
+            private Object _item;
+
+            public override int GetHashCode()
+            {
+                return _category.GetHashCode() + _item.GetHashCode();
+            }
+
+            public override bool Equals(object? obj)
+            {
+                if (obj != null){
+                StoreEntry se = (StoreEntry) obj;
+                return se._item.Equals(this._item) && se._category.Equals(this._category);
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        private Dictionary<StoreEntry, int> _stock;
         
         public StoreService(){
-         _stock = new Dictionary<string, Dictionary<Object, int>>();
-         var itemDict = new Dictionary<Object, int>();
-         _stock.Add("books", itemDict);
-         itemDict.Add(new Isbn(1,2,3,1), 42);
-         itemDict.Add(new Isbn(1,2,3,3), 10);
+         _stock = new Dictionary<StoreEntry, int>();
+         _stock.Add(new StoreEntry("books", new Isbn(1,2,3,1)), 42);
+         _stock.Add(new StoreEntry("books", new Isbn(1,2,3,3)), 10);
         }
 
         public int GetStock(string category, Object item){
             try{
-                Dictionary<Object, int> itemDictionary = _stock[category];
-                int stock = itemDictionary[item];
+                int stock = _stock[new StoreEntry(category, item)];
                 return stock;
             }
             catch(Exception){
